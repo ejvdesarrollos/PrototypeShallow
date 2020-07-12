2e665a28d2bd1fc3e99411d4bb3cde5a0dd67686
Emanuel Villalva <emanuelvillalva@outlook.com.ar>
2020-07-12 20:33:10 -0300

    Agregar archivos de proyecto.

---
+++ PrototypeShallow/ParaClonar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeShallow
+{
+    //Se implementa una Interfaz
+    public class ParaClonar : ICloneable
+    {
+        public string Nombre {get; set; }
+        public int Valor { get; set; }
+
+        //Método que pide implementar por ICloneable
+        public object Clone()
+        {
+            //Es un método de ICloneable que permite crear una copia superficial
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ PrototypeShallow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrototypeShallow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParaClonar oC1 = new ParaClonar();
+            oC1.Nombre = "ejemplo A";
+            oC1.Valor = 10;
+
+            //Observar que por más que oC2 apunte a oC1, en realidad
+            //estoy clonando, es unca copia
+            ParaClonar oC2 = oC1.Clone() as ParaClonar;
+            oC2.Nombre = "ejemplo B";
+
+            //El nombre va a cambiar, pero que hice un Clon, el número al no sobreescribirlo, toma el valor del primero
+            Console.WriteLine("Texto 1: " + oC1.Nombre + " Valor 1: " + oC1.Valor);
+            Console.WriteLine("Texto 2: " + oC2.Nombre + " Valor 2: " + oC2.Valor);
+        }
+    }
+}
